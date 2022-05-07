e699e20d522d277a088abdc254f604bea81a6f35
Kat Marchán <kzm@zkat.tech>
2022-05-07 11:45:34 -0700

    set up tailwindcss and daisyui

--- frontend/src/components/app.rs
+++ frontend/src/components/app.rs
@@ -1,62 +1,9 @@
-use std::collections::HashMap;
-
-use kallisti_error::KallistiError;
-use wasm_bindgen_futures::spawn_local;
-use web_sys::window;
 use yew::prelude::*;
-
-use crate::tauri_api::tauri;
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let welcome = use_state_eq(|| "".to_string());
-    let name = use_state_eq(|| "bl ah".to_string());
-
-    // Execute tauri command via effects.
-    // The effect will run every time `name` changes.
-    {
-        let welcome = welcome.clone();
-        use_effect_with_deps(
-            move |name| {
-                update_welcome_message(welcome, name.clone());
-                || ()
-            },
-            (*name).clone(),
-        );
-    }
-
-    let message = (*welcome).clone();
-
     html! {
-        <div>
-            <h2 class={"heading"}>{message}</h2>
-        </div>
+        <>
+        </>
     }
-}
-
-fn update_welcome_message(welcome: UseStateHandle<String>, name: String) {
-    spawn_local(async move {
-        match hello(&name).await {
-            Ok(message) => {
-                welcome.set(message);
-            }
-            Err(e) => {
-                let window = window().unwrap();
-                window
-                    .alert_with_message(&format!(
-                        "Error: {:?}", e
-                    ))
-                    .unwrap();
-            }
-        }
-    });
-}
-
-async fn hello(name: &str) -> Result<String, KallistiError> {
-    tauri::invoke(
-        "hello",
-        Some(HashMap::from([("name".into(), name.to_string())])),
-    )
-    .await
-    .map(|n| n.as_string().expect("Failed to convert result to string."))
 }
--- frontend/src/tauri_api/tauri.rs
+++ frontend/src/tauri_api/tauri.rs
@@ -10,7 +10,7 @@ extern "C" {
     async fn _invoke(cmd: &str, arg: JsValue) -> Result<JsValue, JsValue>;
 
     #[wasm_bindgen(js_name = "convertFileSrc", js_namespace = ["window", "__TAURI__"])]
-    async fn _convert_file_src(src: &str, protocol: Option<&str>) -> JsValue;
+    fn _convert_file_src(src: &str, protocol: Option<&str>) -> JsValue;
 }
 
 pub async fn invoke<T: Serialize>(
@@ -20,4 +20,12 @@ pub async fn invoke<T: Serialize>(
     _invoke(cmd.as_ref(), JsValue::from_serde(&arg.unwrap_or_default()).expect("Failed to convert arg to JsValue."))
         .await
         .map_err(|e| JsValue::into_serde(&e).expect("Failed to convert error."))
+}
+
+pub fn convert_file_src(src: impl AsRef<str>) -> JsValue {
+    _convert_file_src(src.as_ref(), None)
+}
+
+pub fn convert_file_src_with_protocol(src: impl AsRef<str>, protocol: impl AsRef<str>) -> JsValue {
+    _convert_file_src(src.as_ref(), Some(protocol.as_ref()))
 }
