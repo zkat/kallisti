f391a6d6a6145c07529e33f06f84508c33053bd6
Kat Marchán <kzm@zkat.tech>
2022-05-08 19:50:25 -0700

    cargo fmt

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,8 +2,6 @@ pub mod commands;
 
 pub fn run() -> tauri::Result<()> {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            commands::notify,
-        ])
+        .invoke_handler(tauri::generate_handler![commands::notify,])
         .run(tauri::generate_context!())
 }
