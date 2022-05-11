cd8739fce27f286f5d4435f4e3a18972ec46d7bd
Kat Marchán <kzm@zkat.tech>
2022-05-10 18:11:51 -0700

    basic router stuff

--- frontend/src/components/app.rs
+++ frontend/src/components/app.rs
@@ -1,12 +1,35 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-use crate::components::Login;
+#[derive(Clone, Routable, PartialEq)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Home => html! {
+            <div>
+                <h1>{"Home"}</h1>
+            </div>
+        },
+        Route::NotFound => html! {
+            <div>
+                <h1>{"Not Found"}</h1>
+            </div>
+        },
+    }
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <>
-            <Login />
-        </>
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+        </BrowserRouter>
     }
 }
